a7e92e09eb50d7269e8179c110364a2cbad25d9e
abnerjs <abner.js05@gmail.com>
2021-05-25 18:08:57 -0300

    menu aside init

--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -3,7 +3,13 @@ import './Menu.css'
 
 const Menu = props => (
     <aside className='menu'>
-        comp menu
+        <nav>
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#features">FEATURES</a></li>
+                <li><a href="#prices">PRICES</a></li>
+            </ul>
+        </nav>
     </aside>
 )
 
